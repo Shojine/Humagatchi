6c52e3c147ae83689ed829b7aeae20f2a2f7e102
Shojine <trintentatar@gmail.com>
2026-07-20 11:48:31 -0600

    2D force impulse

--- Assets/Scripts/RagdollController.cs
+++ Assets/Scripts/RagdollController.cs
@@ -1,32 +1,90 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+
+public enum RagdollState
+{
+    Standing, 
+    Fallen, 
+    Slapped,
+    Dead
+}
 public class RagdollController : MonoBehaviour
 {
     [SerializeField] GameObject bodyPart;
-  
+
+    public RagdollState currentState = RagdollState.Standing;
     private Rigidbody[] childRB;
+    private int randomFallingPercentage = 3;
     void Start()
     {
         childRB = transform.GetComponentsInChildren<Rigidbody>();
+        SetRagdollState(currentState);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            SetRagdollState(RagdollState.Slapped);
+        }
+        
     }
 
-    private void DisableBones()
+    
+
+    public void SetRagdollState(RagdollState newState)
     {
-        for (int i = 0; i < childRB.Length; i++)
+        currentState = newState;
+        UpdateRagdollState();
+    }
+
+    private void UpdateRagdollState()
+    {
+        switch(currentState)
         {
-            childRB[i].isKinematic = true;
+            case RagdollState.Standing:
+                EnableBones(false);
+                if (UnityEngine.Random.Range(0, 100) < randomFallingPercentage)
+                {
+                    currentState = RagdollState.Fallen;
+                    break;
+                }
+                    break;
+            case RagdollState.Fallen:
+                EnableBones(true);
+                break;
+            case RagdollState.Slapped:
+                EnableBones(true);
+                Vector3 mouseScreenPoz = Mouse.current.position.ReadValue();
+                mouseScreenPoz.z = Camera.main.WorldToScreenPoint(transform.position).z;
+                Vector3 mouseWorldPoz = Camera.main.ScreenToWorldPoint(mouseScreenPoz);
+                mouseScreenPoz.z = 0;
+                float force = 10;
+
+                Vector2 direction = (transform.position - mouseWorldPoz).normalized;
+                foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+                {
+                    rb.AddForce(direction * force, ForceMode.Impulse);
+                }
+                break;
+            case RagdollState.Dead:
+                EnableBones(true);
+                GetComponent<RagdollController>().enabled = false;
+                break;
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void EnableBones(bool isEnabled)
     {
-        Debug.Log("Collision detected with: " + collision.gameObject.name);
-        bodyPart.GetComponent<Renderer>().material.color = Color.red;
+        foreach (Rigidbody rb in childRB)
+        {
+            rb.isKinematic = !isEnabled;
+        }
     }
+
+
 }
