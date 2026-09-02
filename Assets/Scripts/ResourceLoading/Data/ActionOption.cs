a7e2ce6f1d84a29171922ffb2395f18c88a48f24
tposselt <plutonium221b@gmail.com>
2026-09-02 04:54:22 -0600

    actions list system

---
+++ Assets/Scripts/ResourceLoading/Data/ActionOption.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ActionOption
+{
+    public string name = "";
+    public string type = "";
+    public int cost = 0;
+    public HumanStateChange changes;
+}
+
+[System.Serializable]
+public class ActionCollection
+{
+    public ActionOption[] actions;
+
+    public int FindIndex(ActionOption action)
+    {
+        for (int i = 0; i < actions.Length; i++)
+        {
+            if (actions[i].name == action.name) return i;
+        }
+        return -1;
+    }
+}
